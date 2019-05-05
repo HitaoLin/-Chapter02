1e112e1b44601edb611bcbfc10cee904e7cad8a6
HitaoLin <1127141134@qq.com>
2019-05-05 17:30:40 +0800

    测试1

--- app/src/main/java/com/example/lht/chapter02/MainActivity.java
+++ app/src/main/java/com/example/lht/chapter02/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
